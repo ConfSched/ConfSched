05435ced5981e10bd3275c132504b43ce6bd4ef9
David Crush <dlcrush@mail.roanoke.edu>
2015-02-19 22:42:54 -0500

    added registrationf

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -8,19 +8,49 @@ class UserController extends \BaseController {
 		$member = null;
 
 		if (Input::has('authorid')) {
-			$author = Authors::find(Input::get('authorid'));
+			$author = Authors::findOrFail(Input::get('authorid'));
 		}
 
 		if (Input::has('committeeid')) {
-			$member = Reviewer::find(Input::get('committeeid'));
+			$member = Reviewer::findOrFail(Input::get('committeeid'));
 		}
+
+		if (is_null($author) && is_null($member)) {
+			die('Need a valid author or member id');
+		}
+
+		$first_name = (is_null($author) ? $member->name_first : $author->first_name);
+		$last_name = (is_null($author) ? $member->name_last : $author->last_name);
+		$email = (is_null($author) ? $member->email : $author->email);
 		
 
-		return View::make('register', compact('author', 'member'));
+		return View::make('register', compact('author', 'member', 'first_name', 'last_name', 'email'));
 	}
 
 	public function processRegister() {
-		DBug::DBug(Input::all(), true);
+		//DBug::DBug(Input::all(), true);
+
+		$validator = Validator::make(Input::all(), ['username' => 'required|unique:users', 'password' => 'required', 'password_confirm' => 'required|same:password', 'email' => 'required|unique:users', 'first_name' => 'required', 'last_name' => 'required']);
+
+		if ($validator->fails()) {
+			return Redirect::action('UserController@register', ['committeeid' => Input::get('committeeid', ''), 'authorid' => Input::get('authorid', '')])->withInput()->withErrors($validator);
+		}
+
+		$user = new User;
+		$user->first_name = Input::get('first_name');
+		$user->last_name = Input::get('last_name');
+		$user->email = Input::get('email');
+		$user->username = Input::get('username');
+		$user->password = Hash::make(Input::get('password'));
+		$user->author = Input::has('authorid');
+		$user->author_id = Input::get('authorid', null);
+		$user->committee = Input::has('committeeid');
+		$user->committee_id = Input::get('committeeid', null);
+		$user->save();
+
+		Auth::login($user);
+
+		return Redirect::to('/');
 	}
 
 	/**
--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -25,6 +25,14 @@
     </div>
 
     {{ Form::open(array('class' => 'form', 'method' => 'POST')) }}
+        @if(Input::has('committeeid'))
+            {{ Form::hidden('commiteeid', Input::get('committeeid')) }}
+        @endif
+
+        @if(Input::has('authorid'))
+            {{ Form::hidden('authorid', Input::get('authorid')) }}
+        @endif
+
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="form-group">
@@ -49,28 +57,41 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="form-group">
-                    {{ Form::email('email', Input::old('email', ''), array('placeholder' => 'Email Address', 'class' => 'form-control'))}}
+                    {{ Form::email('email', Input::old('email', $email), array('placeholder' => 'Email Address', 'class' => 'form-control', 'readonly' => 'readonly'))}}
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="form-group">
-                    {{ Form::text('first_name', Input::old('first_name', ''), array('placeholder' => 'First Name', 'class' => 'form-control'))}}
+                    {{ Form::text('first_name', Input::old('first_name', $first_name), array('placeholder' => 'First Name', 'class' => 'form-control', 'readonly' => 'readonly'))}}
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="form-group">
-                    {{ Form::text('last_name', Input::old('last_name', ''), array('placeholder' => 'Last Name', 'class' => 'form-control'))}}
+                    {{ Form::text('last_name', Input::old('last_name', $last_name), array('placeholder' => 'Last Name', 'class' => 'form-control', 'readonly' => 'readonly'))}}
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <div class="form-group">
-                    {{ Form::submit('Sign Up', array('class' => 'btn btn-lg btn-block btn-primary'))}}
+                    @if (is_null($author))
+                        Account Type: Committee Member
+                    @elseif (is_null($member))
+                        Account Type: Author
+                    @else
+                        Account Type: Author & Committee Member
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+                <div class="form-group">
+                    {{ Form::submit('Register', array('class' => 'btn btn-lg btn-block btn-primary'))}}
                 </div>
             </div>
         </div>
