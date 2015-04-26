303d18af11620239497da84d8c017430fdf4c0de
David Crush <dlcrush@mail.roanoke.edu>
2015-04-26 19:29:52 -0400

    fixing view

--- app/views/schedule.blade.php
+++ app/views/schedule.blade.php
@@ -71,12 +71,13 @@
                 <div class="col-xs-6 col-sm-4 col-md-4 col-lg-3">
                   <h3>{{ Date::parse($session->start)->format('n/j') }}</h3>
                   <strong>{{Date::parse($session->start)->format('g:i A')}} - {{Date::parse($session->end)->format('g:i A')}}</strong>
-                </div>
                 <hr style="margin-bottom: 0; margin-top: 0;">
                 @foreach($session->authors as $author)
                   <h5>{{$author->print}}</h5>
                   {{-- <button class="btn btn-default btn-xs btn-swap" data-paper-id="{{$paper->paperid}}">Swap</button> --}}
                 @endforeach
+                </div>
+                <?php $i ++; ?>
             @endforeach
 	@endif
 	<!-- content goes here -->
