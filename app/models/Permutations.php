40a733332a7eb49155b5039f366b8fb2613b45b8
David Crush <dlcrush@mail.roanoke.edu>
2015-04-26 15:43:31 -0400

    no timestamps for permutations

--- app/models/Permutations.php
+++ app/models/Permutations.php
@@ -9,6 +9,7 @@ class Permutations extends \Eloquent {
 	protected $fillable = [];
 	protected $table = 'permutations';
 	protected $appends = ['display_value'];
+	public $timestamps = false;
 
 	protected $primaryKey = null;
 
